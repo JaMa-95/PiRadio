ac092f478727c9306e3f8baaeaef10d4d95dc4bc
Jakob Mattes <jakob.mattes1995@web.de>
2022-10-24 19:56:24 +0200

    Adjust volume

--- Radio/mqtt/mqttClient.py
+++ Radio/mqtt/mqttClient.py
@@ -84,7 +84,7 @@ class MqttClient:
         self.player.stop()
 
     def set_volume(self, volume):
-        volume = int(volume * 2)
+        volume = int(volume * 3)
         if self.player:
             self.player.audio_set_volume(volume)
         self.volume = volume
