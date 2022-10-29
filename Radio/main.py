f4515aedaf8a176e213bc3c873e49220436c5241
Jakob Mattes <jakob.mattes1995@web.de>
2022-10-29 15:09:34 +0200

    Add app

--- Radio/main.py
+++ Radio/main.py
@@ -1,10 +1,11 @@
 from threading import Thread
 from radio import Radio, USBReader
 from audioPlayer import AudioPlayer
+from app import app
 #from web.dataWeb import DataGetter
 
 if __name__ == "__main__":
-
+    app.run()
     usb_reader = USBReader()
     #dataGetter = DataGetter(radio)
     radio = Radio(mqtt=True, play_central=True, play_speaker=True)
