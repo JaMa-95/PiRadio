253c48c1cb25bd19a1642ac6b5bd8d18b89d379a
Jakob Mattes <jakob.mattes1995@web.de>
2022-10-29 15:11:56 +0200

    Add app

--- Radio/app.py
+++ Radio/app.py
@@ -3,7 +3,7 @@ from turbo_flask import Turbo
 import random
 import threading
 import time
-from Radio.db.db import Database
+from db.db import Database
 
 app = Flask(__name__)
 turbo = Turbo(app)
@@ -12,6 +12,7 @@ turbo = Turbo(app)
 @app.route("/")
 def home():
     db = Database()
+    db.create()
     db.insert_volume(10)
     db.insert_stream("abc")
     db.insert_pos_lang_mittel_kurz(21)
@@ -42,18 +43,17 @@ def user(name):
 
 @app.context_processor
 def inject_load():
-    db = Database()
     return {
-        "volume": db.get_volume(),
-        "stream": db.get_stream(),
-        "button_on_off": db.get_button_on_off_web(),
-        "button_lang": db.get_button_lang_web(),
-        "button_mittel": db.get_button_mittel_web(),
-        "button_kurz": db.get_button_kurz_web(),
-        "button_ukw": db.get_button_ukw_web(),
-        "button_spr": db.get_button_spr_mus_web(),
-        "pos_lang_mittel_kurz": db.get_pos_lang_mittel_kurz(),
-        "pos_ukw_spr": db.get_pos_ukw()
+        "volume": random.random(),
+        "stream": random.random(),
+        "button_on_off": random.random(),
+        "button_lang": random.random(),
+        "button_mittel": random.random(),
+        "button_kurz": random.random(),
+        "button_ukw": random.random(),
+        "button_spr": random.random(),
+        "pos_lang_mittel_kurz": random.random(),
+        "pos_ukw_spr": random.random()
     }
 
 
