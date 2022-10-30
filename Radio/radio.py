f9f5bb3769c2b595ae65a8b2a5b8f69b6866af21
Jakob Mattes <jakob.mattes1995@web.de>
2022-10-30 10:49:46 +0100

    Debug

--- Radio/radio.py
+++ Radio/radio.py
@@ -306,6 +306,7 @@ class Radio:
         return changed_hardware
 
     def update_db(self, changed_hardware):
+        print(f"CHANGED HARDWARE: {changed_hardware}")
         if "posLangKurzMittel" in changed_hardware:
             self.db.insert_pos_lang_mittel_kurz(self.current_command["posLangKurzMittel"])
         if "posUKW" in changed_hardware:
