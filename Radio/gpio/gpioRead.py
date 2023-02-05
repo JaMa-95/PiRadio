f1cf2e0c6a09fa0e041b7f8a67ce91b522cb310a
Jakob Mattes <jakob.mattes1995@web.de>
2023-02-05 16:22:22 +0100

    adding gpio

--- Radio/gpio/gpioRead.py
+++ Radio/gpio/gpioRead.py
@@ -9,7 +9,7 @@ BUT_KURZ = 24
 BUT_UKW = 26
 BUT_SPR = 32
 
-buttons = {"BUT_ON": 16, "BUT_LANG": 18, "BUT_MITTEL": 22, "BUT_KURZ": 24, "BUT_UKW": 26, "BUT_SPR": 32}
+buttons = {"BUT_ON": 16}
 
 
 def signal_handler(sig, frame):
@@ -25,7 +25,7 @@ def button_pressed_callback(channel):
 
 if __name__ == '__main__':
     for button_name, button_io in buttons.items():
-
+        print(button_name)
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(button_io, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.add_event_detect(button_io, GPIO.FALLING,
