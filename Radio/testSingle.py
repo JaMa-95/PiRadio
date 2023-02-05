a5374bacbc96abccf81b03066e3ef728efaf05cf
Jakob Mattes <jakob.mattes1995@web.de>
2023-02-05 20:11:39 +0100

    adding gpio

---
+++ Radio/testSingle.py
@@ -0,0 +1,13 @@
+import time
+import vlc
+
+url = "http://184.154.43.106:8243/stream"
+instance = vlc.Instance('--input-repeat=-1', '--fullscreen')
+player = instance.media_player_new()
+media = instance.media_new(url)
+media.get_mrl()
+player.set_media(media)
+player.play()
+time.sleep(10)
+is_playing = player.is_playing()
+print(is_playing)
