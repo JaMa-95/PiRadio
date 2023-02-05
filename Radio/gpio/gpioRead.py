381121ac5650c1f9fc5c87fa0be0ac2a4cae4b61
Jakob Mattes <jakob.mattes1995@web.de>
2023-02-05 16:12:21 +0100

    add all

---
+++ Radio/gpio/gpioRead.py
@@ -0,0 +1,32 @@
+import signal
+import sys
+import RPi.GPIO as GPIO
+
+BUT_ON = 16
+BUT_LANG = 18
+BUT_MITTEL = 22
+BUT_KURZ = 24
+BUT_UKW = 26
+BUT_SPR = 32
+
+buttons = {"BUT_ON": 16, "BUT_LANG": 18, "BUT_MITTEL": 22, "BUT_KURZ": 24, "BUT_UKW": 26, "BUT_SPR": 32}
+
+
+def signal_handler(sig, frame):
+    GPIO.cleanup()
+    sys.exit(0)
+
+
+def button_pressed_callback(channel):
+    print(f"Button pressed! {channel}")
+
+
+if __name__ == '__main__':
+    for button_name, button_io in buttons.items():
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(button_io, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(button_io, GPIO.FALLING,
+                              callback=button_pressed_callback, bouncetime=100)
+
+        signal.signal(signal.SIGINT, signal_handler)
+        signal.pause()
