2e57aed68f2fb2989a4dd1e0037ed01153b1d4f2
Jakob Mattes <jakob.mattes1995@web.de>
2023-02-19 09:58:39 +0100

    test get command string

--- Radio/radio.py
+++ Radio/radio.py
@@ -490,11 +490,10 @@ class USBReader:
             if ser.in_waiting > 1000:
                 ser.flushInput()
             try:
-                command_ = ser.readline().decode("UTF-8")
+                #command_ = ser.readline().decode("UTF-8")
+                command_ = ser.read_until(b';').decode("UTF-8")
                 print("command: " + command_)
-                #command_ = ser.read_until(b';').decode("UTF-8")
                 if command_:
-
                     command_ = command_.replace("\n", "").replace("\r", "")
                     if command_[0] == "-" and command_[-1] == ";":
                         command = command_[1:]
