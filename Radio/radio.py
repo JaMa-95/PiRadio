f2cb9a682a568eca4b7afb91e33b197c9c4afd3c
Jakob Mattes <jakob.mattes1995@web.de>
2023-02-19 15:07:06 +0100

    test get command string

--- Radio/radio.py
+++ Radio/radio.py
@@ -126,7 +126,7 @@ class Radio:
             self.check_esp_reset()
             self.check_change_speakers()
             changed_hardware = self.get_changed_buttons()
-            print("command: " + command)
+            print(f"command: {command}")
             if command != self.currentCommandString:
                 self.set_old_command(self.current_command)
                 self.currentCommandString = command
