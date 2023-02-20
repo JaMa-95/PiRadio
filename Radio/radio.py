66ed2187056ed56e41e0e2f31b0e7bdd4c688c75
Jakob Mattes <jakob.mattes1995@web.de>
2023-02-20 17:51:59 +0100

    Fix led blink

--- Radio/radio.py
+++ Radio/radio.py
@@ -38,7 +38,7 @@ class LedStrip:
         for i in range(self.pixels.count()):
             self.pixels.set_pixel(i, Adafruit_WS2801.RGB_to_color(color[0], color[1], color[2]))
             self.pixels.show()
-            time.sleep(1)
+        time.sleep(1)
         self.pixels.clear()
 
 
