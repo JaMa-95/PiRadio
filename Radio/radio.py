c513cb9d0105e6a33399ae9e0617f9e879d64c75
Jakob Mattes <jakob.mattes1995@web.de>
2023-02-22 19:13:11 +0100

    Add LED class

--- Radio/radio.py
+++ Radio/radio.py
@@ -25,7 +25,7 @@ stop = False
 class LedStrip:
     def __init__(self):
         # LED strip configuration:
-        LED_COUNT = int(20/3)  # Number of LED pixels.
+        LED_COUNT = int(18/3)  # Number of LED pixels.
         LED_PIN = 12  # GPIO pin connected to the pixels (18 uses PWM!).
         # LED_PIN = 10        # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
         LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
