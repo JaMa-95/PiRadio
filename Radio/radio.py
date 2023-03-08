4631558de9f864c2ce59ada12a2a3961b41f0023
Jakob Mattes <jakob.mattes1995@web.de>
2023-03-08 18:48:47 +0100

    new volume formula

--- Radio/radio.py
+++ Radio/radio.py
@@ -323,7 +323,7 @@ class Radio:
 
     def set_volume(self, volume):
         print(f"volume start:{volume}")
-        volume = int(0.0117233 * volume - 116.52)
+        volume = int(0.00588235 * volume - 61.76)
         if volume < 0:
             volume = 0
         elif volume > 100:
