5a835b3e23d0a44234db0ddab2f2181a44b2e03e
Jakob Mattes <jakob.mattes1995@web.de>
2023-03-26 17:57:47 +0200

    Update min max frequ values

--- Radio/radio.py
+++ Radio/radio.py
@@ -184,10 +184,10 @@ class Radio:
                 self.set_volume(self.current_command[changed_hardware])
             else:
                 if changed_hardware in ["posLangKurzMittel", "posUKW"]:
-                    # pass
-                    print("------------------------------------")
-                    print(f"encoder changed {self.current_command['posLangKurzMittel']}, "
-                          f"{self.current_command['posUKW']}")
+                    pass
+                    #print("------------------------------------")
+                    #print(f"encoder changed {self.current_command['posLangKurzMittel']}, "
+                    #      f"{self.current_command['posUKW']}")
                 self.process_hardware_value_change()
 
     @staticmethod
