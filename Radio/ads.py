10472f80a8acea3292224f44741c47b3e5294484
Jakob Mattes <jakob.mattes1995@web.de>
2023-03-26 11:55:14 +0200

    Showing poti status

--- Radio/ads.py
+++ Radio/ads.py
@@ -51,7 +51,7 @@ class AdsSingle:
         values = []
         for i in range(100):
             value = self.chan.value
-            print(f"value now: value")
+            print(f"value now: {value}")
             values.append(value)
 
 
