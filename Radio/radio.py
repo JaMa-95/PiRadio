c82744be7ee75141cb1874e414bca958e0591035
Jakob Mattes <jakob.mattes1995@web.de>
2023-04-02 13:46:40 +0200

    add web control to website

--- Radio/radio.py
+++ Radio/radio.py
@@ -143,6 +143,7 @@ class Radio:
 
                 self.get_command_from_db()
                 changed_hardware = self.get_command_changed()
+                print(f"changed: {changed_hardware}")
                 if changed_hardware:
                     print("hardware changed!!!!!")
                     self.process_hardware_change(changed_hardware)
