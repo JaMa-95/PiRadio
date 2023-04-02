6fac648c4d1f32e0079ce44c8c77e8b185bd0ea9
Jakob Mattes <jakob.mattes1995@web.de>
2023-04-02 12:46:37 +0200

    Extend smoothing

--- Radio/collector.py
+++ Radio/collector.py
@@ -2,15 +2,17 @@ from time import sleep
 
 from button import RadioButtonsRaspi
 from ads import AdsObject
-
+from db import db
 
 class Collector:
     def __init__(self):
         self.buttons = RadioButtonsRaspi()
         self.ads = AdsObject()
+        self.db = db
 
     def run(self):
         while True:
-            self.buttons.set_values_to_db()
-            self.ads.set_to_db()
+            if self.db.get_web_contro_value():
+                self.buttons.set_values_to_db()
+                self.ads.set_to_db()
             sleep(0.005)
