a4824be9e6795e155745cf5d8754e6416a5df27b
Jakob Mattes <jakob.mattes1995@web.de>
2023-04-15 18:26:31 +0200

    Debug flag

--- Radio/radioFrequency.py
+++ Radio/radioFrequency.py
@@ -26,17 +26,14 @@ class RadioFrequency:
 
 
 class Frequencies:
-    def __init__(self):
-        self.frequencies = None
-
     def init_min_max(self):
         number_frequencies = len(self.frequencies)
-        for i in range(number_frequencies):
+        for i in reversed(range(number_frequencies)):
             if i == 0:
-                self.frequencies[i].minimum = max_value_kurz_mittel_lang
+                self.frequencies[i].minimum = min_value
             else:
                 self.frequencies[i].minimum = self.frequencies[i - 1].maximum
-            self.frequencies[i].maximum = int(self.frequencies[i].minimum - (max_value_kurz_mittel_lang - min_value) / \
+            self.frequencies[i].maximum = int(self.frequencies[i].minimum + (max_value_kurz_mittel_lang - min_value) / \
                                               number_frequencies)
 
 
