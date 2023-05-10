0930161ede404fbb5476e85b0672be4b7729695f
Jakob Mattes <jakob.mattes1995@web.de>
2023-05-10 18:06:02 +0200

    test analog second

--- Radio/ads1115/test.py
+++ Radio/ads1115/test.py
@@ -10,11 +10,20 @@ from statistics import mean
 i2c = busio.I2C(board.SCL, board.SDA)  # Create the I2C bus
 ads = ADS.ADS1115(i2c)  # Create the ADC object using the I2C bus
 chan = AnalogIn(ads, ADS.P1)  # Create single-ended input on channel 0
+chan1 = AnalogIn(ads, ADS.P2)  # Create single-ended input on channel 0
 
 while True:
     values = []
     for i in range(50):
         values.append(chan.value)
+    print(max(values))
+    print(min(values))
+    print(mean(values))
+    print(max(values) - min(values))
+    print("---------------")
+    values = []
+    for i in range(50):
+        values.append(chan1.value)
     print(max(values))
     print(min(values))
     print(mean(values))
