017f501a12c04a7f82ba5e7474b6c18d894a3af8
Jakob Mattes <jakob.mattes1995@web.de>
2023-05-20 17:24:27 +0200

    test

--- Radio/gpio/setTrue.py
+++ Radio/gpio/setTrue.py
@@ -2,4 +2,6 @@ import RPi.GPIO as GPIO           # import RPi.GPIO module
 GPIO.setmode(GPIO.BCM)            # choose BCM or BOARD
 GPIO.setup(26, GPIO.OUT) # set a port/pin as an output
 GPIO.output(26, True)       # set port/pin value to 1/GPIO.HIGH/True
+GPIO.setup(4, GPIO.OUT) # set a port/pin as an output
+GPIO.output(4, True)       # set port/pin value to 1/GPIO.HIGH/True
 print(True)
