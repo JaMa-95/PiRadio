9db4c5d26cef0c1a281fa14513f9498ec14c1773
Jakob Mattes <jakob.mattes1995@web.de>
2023-05-21 12:46:04 +0200

    test

--- Radio/radio.py
+++ Radio/radio.py
@@ -320,7 +320,7 @@ class Radio:
         # print(f"VOLUME A: {volume}")
         # volume = int(-(volume - x1) / (x1 - x2) * 100)
         #print(f"VOLUME: {volume}")
-         volume = int(0.00606 * volume - 63.63)
+        volume = int(0.00606 * volume - 63.63)
         if volume < 0:
             volume = 0
         elif volume > 100:
