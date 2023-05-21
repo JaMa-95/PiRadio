077521ef15e9c348a1a0adaa794535315cb2d6e6
Jakob Mattes <jakob.mattes1995@web.de>
2023-05-21 12:24:53 +0200

    test

--- Radio/ads.py
+++ Radio/ads.py
@@ -16,6 +16,7 @@ class AdsObject:
         self.mittel_poti = AdsSingle(3)
 
     def set_to_db(self):
+        self.volume_poti = AdsSingle(2)
         self.volume_poti.set_to_db_smoothed()
         self.mittel_poti = AdsSingle(3)
         self.mittel_poti.set_to_db_smoothed()
