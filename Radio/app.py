7b66114fcff637103dab4371e10f28453c586f32
Jakob Mattes <jakob.mattes1995@web.de>
2023-05-24 20:02:31 +0200

    Delete flask inject

--- Radio/app.py
+++ Radio/app.py
@@ -45,52 +45,6 @@ def user(name):
     return f"Hello {name}!"
 
 
-@app.context_processor
-def inject_load():
-    return {
-        "volume": db.get_volume(),
-        "stream": db.get_stream(),
-        "button_on_off": db.get_button_on_off_web(),
-        "button_lang": db.get_button_lang_web(),
-        "button_mittel": db.get_button_mittel_web(),
-        "button_kurz": db.get_button_kurz_web(),
-        "button_ukw": db.get_button_ukw_web(),
-        "button_spr": db.get_button_spr_mus_web(),
-        "pos_lang_mittel_kurz": 1850, # db.get_pos_lang_mittel_kurz(),
-        "pos_ukw_spr": db.get_pos_ukw(),
-        "radio_name": db.get_radio_name()
-    }
-
-
-#@app.context_processor
-def inject_load_rand():
-    return {
-        "volume": random.randint(0, 100),
-        "stream": db.get_stream(),
-        "button_on_off": random.randint(0, 1),
-        "button_lang": random.randint(0, 1),
-        "button_mittel": random.randint(0, 1),
-        "button_kurz": random.randint(0, 1),
-        "button_ukw": random.randint(0, 1),
-        "button_spr": random.randint(0, 1),
-        "pos_lang_mittel_kurz": random.randint(1850, 3000),
-        "pos_ukw_spr": random.randint(0, 3000),
-        "radio_name": db.get_radio_name()
-    }
-
-
-@app.before_first_request
-def before_first_request():
-    threading.Thread(target=update_load).start()
-
-
-def update_load():
-    with app.app_context():
-        while True:
-            time.sleep(0.5)
-            turbo.push(turbo.replace(render_template('loadavg.html'), 'load'))
-
-
 @app.route('/data')
 def get_data():
     return {
