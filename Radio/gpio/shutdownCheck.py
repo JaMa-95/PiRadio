6c82a7c53af87559d3e829a36096eb39b650f2fc
Jakob Mattes <jakob.mattes1995@web.de>
2023-06-03 13:07:30 +0200

    Test new pins

--- Radio/gpio/shutdownCheck.py
+++ Radio/gpio/shutdownCheck.py
@@ -2,11 +2,11 @@
 
 
 import RPi.GPIO as GPIO
-
+import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 while True:
     GPIO.wait_for_edge(3, GPIO.FALLING)
 
-    print("SHUTDOWN NOW")
+    print(f"SHUTDOWN NOW {time.time()}")
