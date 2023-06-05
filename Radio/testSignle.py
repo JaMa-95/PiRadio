9e015145d5822f09485e3d403f08d85c5dc2761f
Jakob Mattes <jakob.mattes1995@web.de>
2023-06-05 21:17:41 +0200

    Add equalizer

--- Radio/testSignle.py
+++ Radio/testSignle.py
@@ -23,16 +23,19 @@ print(equalizer.get_amp_at_index(5))  # 3 kHz
 print(equalizer.get_amp_at_index(6))  # 6 kHz
 print(equalizer.get_amp_at_index(7))  # 12 kHz
 
-equalizer.set_amp_at_index(0, 20)  # 60 Hz
-equalizer.set_amp_at_index(1, 20)  # 170 Hz
-equalizer.set_amp_at_index(2, 20)  # 310 Hz
-equalizer.set_amp_at_index(3, 20)  # 600 Hz
-equalizer.set_amp_at_index(4, 20)  # 1 kHz
-equalizer.set_amp_at_index(5, 20)  # 3 kHz
-equalizer.set_amp_at_index(6, 20)  # 6 kHz
-equalizer.set_amp_at_index(7, 20)  # 12 kHz
+print("------------------")
+print(equalizer.set_amp_at_index(0, 20)) # 60 Hz
+equalizer.set_amp_at_index(1, -20)  # 170 Hz
+equalizer.set_amp_at_index(2, -20)  # 310 Hz
+equalizer.set_amp_at_index(3, -20)  # 600 Hz
+equalizer.set_amp_at_index(4, -20)  # 1 kHz
+equalizer.set_amp_at_index(5, -20)  # 3 kHz
+equalizer.set_amp_at_index(6, -20)  # 6 kHz
+equalizer.set_amp_at_index(7, -20)  # 12 kHz
 player.set_equalizer(equalizer)
 print("eualizer set")
+print("------------------")
+
 
 print(equalizer.get_amp_at_index(0))  # 60 Hz
 print(equalizer.get_amp_at_index(1))  # 170 Hz
