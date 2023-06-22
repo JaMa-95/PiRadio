981855fadfd44dabd344d39e510b0f4203468527
Jakob Mattes <jakob.mattes1995@web.de>
2023-06-22 19:54:02 +0200

    Add cleanup gpio

--- Radio/gpio/setTrue.py
+++ Radio/gpio/setTrue.py
@@ -4,7 +4,8 @@ GPIO.cleanup()
 
 GPIO.setmode(GPIO.BCM)            # choose BCM or BOARD
 GPIO.setup(26, GPIO.OUT) # set a port/pin as an output
-GPIO.output(26, True)       # set port/pin value to 1/GPIO.HIGH/True
+GPIO.output(26, GPIO.HIGH)       # set port/pin value to 1/GPIO.HIGH/True
+
 GPIO.setup(4, GPIO.OUT) # set a port/pin as an output
 GPIO.output(4, True)       # set port/pin value to 1/GPIO.HIGH/True
 
