f91be51066a8bfcbdc6597b659f1662c65b46493
Jakob Mattes <jakob.mattes1995@web.de>
2023-07-02 14:07:51 +0200

    Fix cycle time

--- Radio/radio.py
+++ Radio/radio.py
@@ -180,9 +180,8 @@ class Radio:
                     self.process_hardware_value_change()
                     self.old_command = self.current_command
             sleep_time = self.cycle_time - (time.time() - start)
-            print(f"sleep time: {sleep_time}")
             if sleep_time <= 0:
-                print(f"A: {self.cycle_time} : {time.time() - start}")
+                # needs at least some cycle time for other processes
                 time.sleep(0.00001)
             else:
                 time.sleep(sleep_time)
