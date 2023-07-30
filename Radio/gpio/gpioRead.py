c579645a6e016e358ba7b6f1c08db3d9dec451c8
Jakob Mattes <jakob.mattes1995@web.de>
2023-07-30 17:02:37 +0200

    Test more buttons

--- Radio/gpio/gpioRead.py
+++ Radio/gpio/gpioRead.py
@@ -25,6 +25,14 @@ GPIO.setup(21, GPIO.OUT)
 
 GPIO.setup(26, GPIO.OUT)
 
+GPIO.setup(4, GPIO.OUT)
+
+GPIO.setup(17, GPIO.OUT)
+
+GPIO.setup(27, GPIO.OUT)
+
+GPIO.setup(22, GPIO.OUT)
+
 try:
     while True:
         if (GPIO.input(23) == GPIO.HIGH):
@@ -39,6 +47,15 @@ try:
             print("Pin 21 button is pressed")
         if (GPIO.input(26) == GPIO.HIGH):
             print("Pin 26 button is pressed")
+
+        if (GPIO.input(4) == GPIO.HIGH):
+            print("Pin 4 button is pressed")
+        if (GPIO.input(17) == GPIO.HIGH):
+            print("Pin 17 button is pressed")
+        if (GPIO.input(27) == GPIO.HIGH):
+            print("Pin 27 button is pressed")
+        if (GPIO.input(22) == GPIO.HIGH):
+            print("Pin 22 button is pressed")
         time.sleep(0.01)
 
 except KeyboardInterrupt:
