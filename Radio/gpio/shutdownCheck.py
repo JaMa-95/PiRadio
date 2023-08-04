d929b591890d5a35927f7dc9fe07830d6bb6f775
Jakob Mattes <jakob.mattes1995@web.de>
2023-08-04 18:21:49 +0200

    Test

--- Radio/gpio/shutdownCheck.py
+++ Radio/gpio/shutdownCheck.py
@@ -7,7 +7,7 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 while True:
-    GPIO.wait_for_edge(3, GPIO.FALLING)
+    GPIO.wait_for_edge(6, GPIO.FALLING)
 
     print(f"SHUTDOWN NOW {time.time()}")
     time.sleep(0.1)
