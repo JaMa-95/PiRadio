82f8de390f85cc60bd224ab856375dbe5ee13da1
Jakob Mattes <jakob.mattes1995@web.de>
2023-08-13 17:40:44 +0200

    Test

--- Radio/db/db.py
+++ Radio/db/db.py
@@ -1,20 +1,7 @@
 import sqlite3
 import threading
 
-
-class Singleton:
-    _instance = None
-    _lock = threading.Lock()
-
-    def __new__(cls, *args, **kwargs):
-        if not cls._instance:
-            with cls._lock:
-                # another thread could have created the instance
-                # before we acquired the lock. So check that the
-                # instance is still nonexistent.
-                if not cls._instance:
-                    cls._instance = super(Singleton, cls).__new__(cls)
-        return cls._instance
+from Radio.util.singleton import Singleton
 
 
 class Database(Singleton):
