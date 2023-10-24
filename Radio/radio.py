a234cb6147e0cabc0dbf321156112492d2960541
Jakob Mattes <jakob.mattes1995@web.de>
2023-10-24 17:16:05 +0200

    Add data

--- Radio/radio.py
+++ Radio/radio.py
@@ -336,6 +336,7 @@ class Radio:
             volume = 0
         elif volume > 100:
             volume = 100
+        print(f"Volume: {volume}")
         self.volume_old = volume
         self.db.replace_volume(volume)
         self.send_volume(volume)
