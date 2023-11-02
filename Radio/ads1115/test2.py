085179c5b74e84594f38ed9a435524c45f41de80
Jakob Mattes <jakob.mattes1995@web.de>
2023-11-02 10:02:36 +0100

    Add

--- Radio/ads1115/test2.py
+++ Radio/ads1115/test2.py
@@ -4,11 +4,14 @@ import adafruit_ads1x15.ads1115 as ADS
 import board
 import busio
 from adafruit_ads1x15.analog_in import AnalogIn
+from adafruit_ads1x15.ads1x15 import Mode
 
 from statistics import mean
 
 i2c = busio.I2C(board.SCL, board.SDA)  # Create the I2C bus
 ads = ADS.ADS1115(i2c)  # Create the ADC object using the I2C bus
+ads.data_rate = 860
+ads.mode = Mode.CONTINUOUS
 chan = AnalogIn(ads, ADS.P0)  # Create single-ended input on channel 0
 chan1 = AnalogIn(ads, ADS.P1)  # Create single-ended input on channel 0
 chan2 = AnalogIn(ads, ADS.P2)  # Create single-ended input on channel 0
@@ -18,12 +21,25 @@ pins = [ADS.P0, ADS.P1, ADS.P2, ADS.P3]
 
 while True:
     values = []
-    for i in range(20):
+    num_values = 800
+
+    start = time.time()
+    for i in range(num_values):
         values.append(chan3.value)
-        time.sleep(0.1)
+
+    #  delete min man values
+    for _ in range(10):
+        for _ in range(int(num_values / 10)):
+            values.remove(max(values))
+            values.remove(min(values))
+        else:
+            break
+    end = time.time()
+    # if high_precision:
+    #    time_end = time.time()
+    print(f"DURATION: {end - start}")
     print(f"max: {max(values)}")
     print(f"min: {min(values)}")
     print(f"mean: {mean(values)}")
     print(f"range: {max(values) - min(values)}")
     print("---------------")
-    time.sleep(0.2)
