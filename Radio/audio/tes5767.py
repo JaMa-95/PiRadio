059489eac3c24f5178a5a8996cb0e3393eea0645
Jakob Mattes <jakob.mattes1995@web.de>
2024-06-09 11:23:54 +0200

    Imlementation of tea finished

--- Radio/audio/tes5767.py
+++ Radio/audio/tes5767.py
@@ -1,19 +1,20 @@
-#!/usr/bin/python3
-#Reference code obtained from "https://www.raspberrypi.org/forums/viewtopic.php?t=53680" by user: LinuxCircle
 import json
+import sys
+
 from Radio.util.dataTransmitter import Subscriber
-import smbus as smbus 
 import subprocess
-import curses
-import curses.textpad
 import time
-from Radio.util.util import get_project_root
+from Radio.util.util import get_project_root, is_raspberry
 from Radio.util.dataTransmitter import Publisher
 
+if is_raspberry():
+    import smbus as smbus
+
+
 class FmModule(Subscriber):
-    def __init__(self, publisher: Publisher):
-        self.i2c = smbus.SMBus(1)
+    def __init__(self, publisher: Publisher = Publisher()):
         self.i2c_address: int = 0x60
+        self.i2c: smbus.SMBus = None
         self.frequency = 0
         self.active: bool = False
         self._init_fm_module()
@@ -22,6 +23,7 @@ class FmModule(Subscriber):
 
     def _init_fm_module(self):
         if self.active:
+            self.i2c = smbus.SMBus(1)
             self.i2c.write_quick(self.i2c_address)
             print("FM module initialized")
 
@@ -33,8 +35,8 @@ class FmModule(Subscriber):
         self.active = settings["audio"]["active"]
 
     def __del__(self):
-        self.i2c.close()
-        curses.endwin()
+        if self.active:
+            self.i2c.close()
         print("FM module stopped")
 
     def update(self):
@@ -52,22 +54,22 @@ class FmModule(Subscriber):
 
     def set_freq(self, fm_frequency):
         """set Radio to specific frequency"""
-        freq14bit = int (4 * (fm_frequency * 1000000 + 225000) / 32768) # Frequency distribution for two bytes (according to the data sheet)
-        freqH = freq14bit>>8 #int (freq14bit / 256)
+        freq14bit = int(4 * (
+                fm_frequency * 1000000 + 225000) / 32768)  # Frequency distribution for two bytes (according to the data sheet)
+        freqH = freq14bit >> 8  #int (freq14bit / 256)
         freqL = freq14bit & 0xFF
 
-        data = [0 for i in range(4)] # Descriptions of individual bits in a byte - viz.  catalog sheets
-        init = freqH # freqH # 1.bajt (MUTE bit; Frequency H)  // MUTE is 0x80
-        data[0] = freqL # 2.bajt (frequency L)
-        data[1] = 0xB0 #0b10110000 # 3.bajt (SUD; SSL1, SSL2; HLSI, MS, MR, ML; SWP1)
-        data[2] = 0x10 #0b00010000 # 4.bajt (SWP2; STBY, BL; XTAL; smut; HCC, SNC, SI)
-        data[3] = 0x00 #0b00000000 # 5.bajt (PLREFF; DTC; 0; 0; 0; 0; 0; 0)
+        data = [0 for i in range(4)]  # Descriptions of individual bits in a byte - viz.  catalog sheets
+        init = freqH  # freqH # 1.bajt (MUTE bit; Frequency H)  // MUTE is 0x80
+        data[0] = freqL  # 2.bajt (frequency L)
+        data[1] = 0xB0  #0b10110000 # 3.bajt (SUD; SSL1, SSL2; HLSI, MS, MR, ML; SWP1)
+        data[2] = 0x10  #0b00010000 # 4.bajt (SWP2; STBY, BL; XTAL; smut; HCC, SNC, SI)
+        data[3] = 0x00  #0b00000000 # 5.bajt (PLREFF; DTC; 0; 0; 0; 0; 0; 0)
         try:
-            self.i2c.write_i2c_block_data(self.address, init, data) # Setting a new frequency to the circuit
+            self.i2c.write_i2c_block_data(self.address, init, data)  # Setting a new frequency to the circuit
             print("Frequency set to: " + str(fm_frequency))
         except IOError:
             subprocess.call(['i2cdetect', '-y', '1'])
-
 
     def mute(self):
         """"mute radio"""
@@ -88,47 +90,45 @@ class FmModule(Subscriber):
 
 if __name__ == '__main__':
     fm_module = FmModule()
-    frequency = 101.1 # sample starting frequency
+    frequency = 101.1  # sample starting frequency
     # terminal user input infinite loop
-    stdscr = curses.initscr()
-    curses.noecho()
     try:
         while True:
-            c = stdscr.getch()
-            if c == ord('f'): # set to 101.1
+            print("Write command:")
+            c = input()
+            print(c)
+            if c == 'f':  # set to 101.1
                 frequency = 106.9
                 fm_module.set_freq(frequency)
                 time.sleep(1)
-            elif c == ord('v'): # set to 102.1
+            elif c == 'v':  # set to 102.1
                 frequency = 102.1
                 fm_module.set_freq(frequency)
                 time.sleep(1)
-            elif c == ord('w'): # increment by 1
+            elif c == 'w':  # increment by 1
                 frequency += 1
                 fm_module.set_freq(frequency)
                 time.sleep(1)
-            elif c == ord('s'): # decrement by 1
+            elif c == 's':  # decrement by 1
                 frequency -= 1
                 fm_module.set_freq(frequency)
                 time.sleep(1)
-            elif c == ord('e'): # increment by 0.1
+            elif c == 'e':  # increment by 0.1
                 frequency += 0.1
                 fm_module.set_freq(frequency)
                 time.sleep(1)
-            elif c == ord('d'): # decrement by 0.1
+            elif c == 'd':  # decrement by 0.1
                 frequency -= 0.1
                 fm_module.set_freq(frequency)
                 time.sleep(1)
-            elif c == ord('m'): # mute
+            elif c == 'm':  # mute
                 fm_module.mute()
                 time.sleep(1)
-            elif c == ord('u'): # unmute
+            elif c == 'u':  # unmute
                 fm_module.set_freq(frequency)
                 time.sleep(1)
-            elif c == ord('q'): # exit script and cleanup
+            elif c == 'q':  # exit script and cleanup
                 fm_module.mute()
-                curses.endwin()
                 break
     except KeyboardInterrupt:
         fm_module.mute()
-        curses.endwin()
