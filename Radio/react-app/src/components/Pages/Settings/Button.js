0ceab8d663cfe10d6b8f2adc6427dccee58a62d1
Jakob Mattes <jakob.mattes1995@web.de>
2024-09-04 18:13:48 +0200

    Add actions

--- Radio/react-app/src/components/Pages/Settings/Button.js
+++ Radio/react-app/src/components/Pages/Settings/Button.js
@@ -1,6 +1,6 @@
 import './Button.css'
-import { useState } from 'react';
-
+import { useEffect, useState } from 'react';
+import Dropdown from '../../../commen/Dropdown';
 
 export default function Button(props) {
     const [name, setName] = useState(props.name);
@@ -12,15 +12,38 @@ export default function Button(props) {
     const [isChangeSpeaker, setIsChangeSpeaker] = useState(props.settings.is_change_speaker);
     const [freq, setFreq] = useState(props.settings.freq);
     const [action, setAction] = useState(props.settings.action);
+    const [type, setType] = useState("");
+    const types = ["ON/OFF Music", "ON/OFF Raspi", "Change Speaker", "Frequency"];
+    const actions = {
+        TURN_OFF_RASPBERRY: 0,
+        STOP_MUSIC: 1,
+        PLAY_MUSIC: 2,
+        HOLD_FREQUENCY: 3,
+        ROTATE_AUDIO_SOURCE: 4,
+        SET_AUDIO_SOURCE: 5
+    };
+    const clickTypes = {
+        BUTTON_STATE_OFF: 0,
+        BUTTON_STATE_ON: 1,
+        BUTTON_STATE_SHORT_CLICK: 2,
+        BUTTON_STATE_LONG_CLICK: 3,
+        BUTTON_STATE_DOUBLE_CLICK: 4,
+        BUTTON_STATE_TO_ON: 5,
+        BUTTON_STATE_TO_OFF: 6
+    };
 
-    console.log("Button: ", props.settings);
-    console.log("active: ", active);
-
-    const renderFreq = () => {
-        if (props.freq) {
-            return <Frequency />;
+    useEffect(() => {
+        if (isOnOff) {
+            setType("Mute");
+        } else if (isOnOffRaspi) {
+            setType("ON/OFF Raspi");
+        } else if (isChangeSpeaker) {
+            setType("Change Speaker");
+        } else {
+            setType("Play Music");
         }
-    }
+    }, [isOnOff, isOnOffRaspi, isChangeSpeaker, freq]);
+
     return (
         <div className="button">
             <input type="checkbox" name='active' id='active' checked={active} onChange={(e) => setActive(e.target.value)} />
@@ -38,8 +61,29 @@ export default function Button(props) {
                 </div>
             </section>
             <section>
-                {renderFreq()}
+                <label for="type" className='typeLabel'> Type: <b>{type}</b> </label>
+                <div className="dropdown">
+                    <Dropdown
+                        trigger={<button>Click to set type</button>}
+                        menu={
+                            types.map((type, index) => (
+                                <button key={index} onClick={() => setType(type)}>
+                                    {type}
+                                </button>
+                            ))
+
+                        }
+                    />
+                </div>
             </section>
+            <section>
+                <Action />
+            </section>
+            {type === "Play Music" && (
+                <section>
+                    <Frequency />
+                </section>
+            )}
             <div class="vertical-center">
                 <button type="button" className="Delete">Delete</button>
             </div>
@@ -50,12 +94,42 @@ export default function Button(props) {
     );
 };
 
+function Action() {
+    return (
+        <div>
+            <button>Add action</button>
+            <div>
+                <button>Add apply state</button>
+                <ul>
+
+                </ul>
+                <li></li><button>Remove action</button>
+            </div>
+        </div>
+    )
+}
+
+function ApplyState() {
+    return (
+        <ul>
+            clickTypes.map((clickType, index) => (
+            <li key={index}>{clickType}</li>
+            ))
+        </ul>
+    )
+}
+
 function Frequency() {
     return (
         <div>
             <div>
                 <label for="freqPos">Frequency position</label>
-                <select id="freqPos" name="freqPos" />
+                <select id="freqPos" name="freqPos" >
+                    <option value="volvo">Volvo</option>
+                    <option value="saab">Saab</option>
+                    <option value="mercedes">Mercedes</option>
+                    <option value="audi">Audi</option>
+                </select>
             </div>
             <div>
                 <label for="freqMusList">Frequency music list</label>
