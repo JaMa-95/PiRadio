e3511e1f44890b985a4e65bf84312527ff162843
Jakob Mattes <jakob.mattes1995@web.de>
2024-11-22 21:23:09 +0100

    Sudo shutdown

--- Radio/startupButton/onOffButton.py
+++ Radio/startupButton/onOffButton.py
@@ -77,7 +77,7 @@ class OnOffButton:
             print ("Starting...\n")
             GPIO.cleanup()
             time.sleep(1)
-            #GPIO.setmode(GPIO.BCM)
+            GPIO.setmode(GPIO.BCM)
 
             GPIO.setup(PIN, GPIO.OUT)
             GPIO.output(PIN, GPIO.HIGH)
